bc822258e812736b62eaf5006418034dc36a551f
dhrumil0902 <64914881+dhrumil0902@users.noreply.github.com>
2025-01-10 21:11:13 -0500

    configured chatbot backend

--- chatbot_backend/chatbot/views.py
+++ chatbot_backend/chatbot/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import openai
+from openai import OpenAI
 
-openai.api_key = "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"
-
+client = OpenAI(
+  api_key="sk-svcacct-21wZQaBc8_ukUENTNQ9msw1gPIrQujyDXVK1bWIOKEaoA-D_AWCvZespSTceKp9T3BlbkFJh0iMq6w89rIzG6Ebw03ccdWlmZS8t5SE1wSULmDa_WXLtcJQmiVTwLKIV4nrEAA"
+)
 class ChatbotAPIView(APIView):
     def post(self, request):
         user_message = request.data.get("message")
@@ -13,14 +14,15 @@ class ChatbotAPIView(APIView):
 
         try:
             # Call OpenAI API to generate a response
-            response = openai.ChatCompletion.create(
-                model="gpt-4",
+            response = client.chat.completions.create(
+                model="gpt-4o-mini",
                 messages=[
-                    {"role": "system", "content": "You are Dhrumil, a software engineer. Answer questions about your resume, projects, and interests."},
+                    {"role": "system", "content": ""},
                     {"role": "user", "content": user_message},
                 ],
             )
-            reply = response['choices'][0]['message']['content']
+            reply = response.choices[0].message
+            print(reply)
             return Response({"response": reply}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
